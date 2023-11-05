0fbe7b364a0e494b93c9bb2ba5d63744f2857742
Hojoon Kim <hojoon0724@gmail.com>
2023-11-04 19:11:50 -0700

    get even answered

--- answers.js
+++ answers.js
@@ -8,8 +8,14 @@ console.log("sup");
 // -----------------------------------------------------
 // Part II: JS Reps
 // -----------------------------------------------------
-
+//* Easy Going
 //? Write a loop, log 1-20
 for (i = 0; i < 20; i++) {
   console.log("number is", i + 1);
+}
+
+//* Get Even
+//? Write loop: even nums 0-200
+for (i = 0; i <= 200; i += 2) {
+  console.log("number is", i);
 }
